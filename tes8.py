ffc2d6e9498f100bcbe9746c60b83b7a77b96803
PSV <umito.kirchwehm@tum.de>
2025-01-29 15:34:07 +0100

    29.01.25 v1

---
+++ tes8.py
@@ -0,0 +1,19 @@
+import win32api
+import win32com.client
+import time
+AcquisitionInstance = win32com.client.Dispatch('PSV.AcquisitionInstance')
+#print(app)
+#print(dir(app))
+app=AcquisitionInstance.GetApplication(True, 10000)
+print(app)
+print(dir(app))
+app.Application.Activate()
+#start acquisition
+app.Acquisition.Start(1)
+#app.Application.Acquisition.GeneratorsOn=False #generator an oder aus machen mit True oder False
+#vllt am ende wir es gebraucht um laser auszuschalten
+#app.Application.Acquisition.Scan(0)
+for i in range(5):
+    print(type(app.Application.Acquisition.State))
+    print(app.Application.Acquisition.State)
+    timesleep()
