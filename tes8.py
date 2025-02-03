ef1dde9686932241472bf40e68fc86e35fdb5edd
PSV <umito.kirchwehm@tum.de>
2025-02-03 15:01:56 +0100

    15.01.v1

--- tes8.py
+++ tes8.py
@@ -14,16 +14,21 @@ app.Application.Activate()
 #app.Acquisition.Start(1)
 #app.Application.Acquisition.GeneratorsOn=False #generator an oder aus machen mit True oder False
 #vllt am ende wir es gebraucht um laser auszuschalten
-app.Application.Acquisition.Scan(0)
+#app.Application.Acquisition.Scan(0)
+app.Application.Acquisition.ScanFileName.title()
+print(app.Application.Acquisition.ScanFileName.title())
 #app.Application.Acquisition.Start
 status=app.Application.Acquisition.State
-while status==3: #wenn gescanned wird ist status=3
-    status = app.Application.Acquisition.State
-    print(type(status))
-    print(status)
-    time.sleep(2)
-print(type(app.Application.Acquisition.Document.Save))
-print(app.Application.Acquisition.Document.Save)
-print(dir(app.Application.Acquisition.ScanFileName))
-app.Application.Acquisition.Document.Save
-
+#while status==3: #wenn gescanned wird ist status=3
+#    status = app.Application.Acquisition.State
+#    print(type(status))
+#    print(status)
+#    time.sleep(2)
+print(dir(app.Application.Acquisition.Infos))
+print(type(app.Application.Acquisition.Infos.GetIDsOfNames))
+print(app.Application.Acquisition.Infos.GetIDsOfNames)
+print(dir(app.Application.Acquisition.ScanFileName.title().replace))
+dokument=app.Application.Acquisition.ScanFileName.title().replace(app.Application.ActiveDocument.Name, app.Application.Acquisition.ScanFileName.title())
+#app.Application.Acquisition.Document.Save.Title
+print(app.Application.ActiveDocument.Name)
+print(dokument)
